979afed2fda7a5ca941f45001ce2219a060dcaa5
webLashchuk$ git config --global user.name webLashchukgit config --global user.name webLashchuk <chernyadyeviv@gmail.com>
2022-12-05 12:56:46 +0200

    added gallery

--- js/main.js
+++ js/main.js
@@ -1,13 +1,51 @@
 $(function(){
-    const headerHeight = $('.header').outerHeight();
+    // const headerHeight = $('.header').outerHeight();
 
-    $(window).scroll(function () {
-        if ($(window).scrollTop() >= headerHeight) {
-            $('.header').addClass('header__sticky')
-        } else {
-            $('.header').removeClass('header__sticky')
+    // $(window).scroll(function () {
+    //     if ($(window).scrollTop() >= headerHeight) {
+    //         $('.header').addClass('header__sticky')
+    //     } else {
+    //         $('.header').removeClass('header__sticky')
+    //     }
+    // });
+
+    Fancybox.bind("[data-fancybox='gallery']", {
+        Toolbar  : false,
+        selector : '.swiper-slide:not(.swiper-slide-duplicate)',
+        animated: false,
+        showClass: false,
+        hideClass: false,
+        infinite: false,
+        closeButton: 'top',
+        hideScrollbar: true,
+        infinite: false,
+        loop: false,
+        mainClass: 'products-gallery',
+        backFocus : false,
+    
+        Image: {
+            click: 'close',
+            wheel: 'slide',
+            zoom: false,
+            fit: "contain",
         }
     });
 
-    var mixer = mixitup('.portfolio__content');
+    const containerEl = document.querySelector('.portfolio__content');
+    let mixer;
+
+    if (containerEl) {
+        mixer = mixitup(containerEl, {
+            controls: {
+                toggleDefault: 'none'
+            },
+            classNames: {
+                block: '',
+                elementFilter: 'portfolio__filter-btn'
+            }
+        });
+    }
+
+    // var mixer = mixitup('.portfolio__content');
+
 })
